8e222dd912a59e3e407b26fb52c720993924c572
doxh23 <adrien.peters95@gmail.com>
2021-10-11 14:40:51 +0200

    final version

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const { getCode, getName } = require('country-list');
 const args = process.argv.slice(2)
 
 let countrycode = getCode(args[0])
-let year = args[1]
-console.log(countrycode)
+
+
 async function getUser() {
     try {
-      const response = await axios.get(`https://date.nager.at/api/v3/publicholidays/${year}/${countrycode}`);
+      const response = await axios.get(`https://date.nager.at/api/v3/NextPublicHolidays/${countrycode}`);
       let datas = response.data
      datas.forEach(country => { 
           
