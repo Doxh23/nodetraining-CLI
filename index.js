78955f6800377614d150d6295e617c1620ed8d29
doxh23 <adrien.peters95@gmail.com>
2021-10-11 12:28:32 +0200

    publish

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello,Node.js")
+#!/usr/bin/env node
+const axios = require('axios');
+const { getCode, getName } = require('country-list');
+const args = process.argv.slice(2)
+
+let countrycode = getCode(args[0])
+let year = args[1]
+console.log(countrycode)
+async function getUser() {
+    try {
+      const response = await axios.get(`https://date.nager.at/api/v3/publicholidays/${year}/${countrycode}`);
+      let datas = response.data
+     datas.forEach(country => { 
+          
+     
+      console.log(country.date +"///" + country.name + "///" + country.localName);
+    });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  getUser()
